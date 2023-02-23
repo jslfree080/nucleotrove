2f2480f9fb859cd4a8e1ba88833ea0bfc0295303
jslit <jslit@jungsoo080ui-MacBookPro.local>
2023-02-23 17:26:38 +0900

    strings and segments

---
+++ src/main/kotlin/com/jslfree080/nucleotrove/StringsAndSegments3.kt
@@ -0,0 +1,18 @@
+package com.jslfree080.nucleotrove
+
+fun main() {
+    val nucleotides = "AGTCGTGNNANCT".toCharArray()
+    val lesser = mutableListOf<Char>()
+    val greater = mutableListOf<Char>()
+
+    /* compare characters using their Unicode values */
+    nucleotides.forEach {
+        if (it < 'G') {
+            lesser.add(it)
+        } else if (it > 'G') {
+            greater.add(it)
+        }
+    }
+    println("Lesser nucleotides: $lesser") // Lesser nucleotides: [A, C, A, C]
+    println("Greater nucleotides: $greater") // Greater nucleotides: [T, T, N, N, N, T]
+}
